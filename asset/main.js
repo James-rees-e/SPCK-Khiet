cedaacfa7e1f5c8755caccd5e0d5f398144ac484
maidzui123 <maiduy190802@gmail.com>
2024-01-07 12:38:27 +0700

    Chỉnh lại design thêm icon màu mè vào cho đẹp, chỉnh tên trang web gắn thêm logo website, test lại chức năng login, sign up

--- asset/main.js
+++ asset/main.js
@@ -1,73 +1,91 @@
-const hook = document.getElementById("thehook")
-const get = document.getElementById("get")
-const fill = document.getElementById("thefill")
-
-
-var imageUrls = ['./img/omg.jpg', './img/fish.jpg', './img/wohohohoho.jpg'];
+const hook = document.getElementById("thehook");
+const get = document.getElementById("get");
+const fill = document.getElementById("fillOuter");
+const selectLimit = document.getElementById("selectLimit");
+const selectType = document.getElementById("selectType");
+var imageUrls = ["./img/omg.jpg", "./img/fish.jpg", "./img/wohohohoho.jpg"];
 var randomIndex = Math.floor(Math.random() * imageUrls.length);
 
-document.getElementById("mainContainer").style.backgroundImage = 'url(' + imageUrls[randomIndex];
-
-
-
-
-
-
-
-const jokes = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'dc8409eca5mshb2bb71c3f40e74cp183740jsn0b1cb69a9fbf',
-		'X-RapidAPI-Host': 'jokes-by-api-ninjas.p.rapidapi.com'
-	}
-};
-
-fetch('https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes?limit=2', jokes)
-	.then(res => res.json())
-	.then(data => {
-		console.log(data);
-		// Xử lý dữ liệu về truyện cười ở đây
-	})
-	.catch(error => console.error('Error fetching jokes data:', error));
-
-const trivia = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'dc8409eca5mshb2bb71c3f40e74cp183740jsn0b1cb69a9fbf',
-		'X-RapidAPI-Host': 'trivia-by-api-ninjas.p.rapidapi.com'
-	}
-};
-
-fetch('https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia', trivia)
-	.then(res => res.json())
-	.then(data => {
-		console.log(data);
-		// Xử lý dữ liệu về truyện cười ở đây
-	})
-	.catch(error => console.error('Error fetching trivia data:', error));
-
-
-
+document.getElementById("generatorOuter").style.backgroundImage =
+  "url(" + imageUrls[randomIndex];
 
 get.addEventListener("click", function () {
-	fill.innerHTML = null;
-	const facts = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'dc8409eca5mshb2bb71c3f40e74cp183740jsn0b1cb69a9fbf',
-			'X-RapidAPI-Host': 'facts-by-api-ninjas.p.rapidapi.com'
-		}
-	};
+  console.log(selectLimit.value);
+  fill.innerHTML = null;
+  const facts = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "dc8409eca5mshb2bb71c3f40e74cp183740jsn0b1cb69a9fbf",
+      "X-RapidAPI-Host": "facts-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+  const jokes = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "dc8409eca5mshb2bb71c3f40e74cp183740jsn0b1cb69a9fbf",
+      "X-RapidAPI-Host": "jokes-by-api-ninjas.p.rapidapi.com",
+    },
+  };
 
-	fetch(`https://facts-by-api-ninjas.p.rapidapi.com/v1/facts?limit=5`, facts)
-		.then(res => res.json())
-		.then((data) => {
-			console.log("fact", data)
-			// hook.innerHTML = 
-			for (i in data) {
-				fill.insertAdjacentHTML("beforeend",
-					`<p>${data[i].fact}</p>`
-				)
-			}
-		})
-})
+  const trivia = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "dc8409eca5mshb2bb71c3f40e74cp183740jsn0b1cb69a9fbf",
+      "X-RapidAPI-Host": "trivia-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+  if (selectLimit.value !== "Amount") {
+    document.getElementById("answerBtn").classList.add("hide");
+    if (selectType.value === "facts") {
+      fetch(
+        `https://facts-by-api-ninjas.p.rapidapi.com/v1/facts?limit=${selectLimit.value}`,
+        facts
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("fact", data);
+          // hook.innerHTML =
+          for (i in data) {
+            fill.insertAdjacentHTML("beforeend", `<li>${data[i].fact}</li>`);
+          }
+        });
+    } else if (selectType.value === "jokes") {
+      fetch(
+        `https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes?limit=${selectLimit.value}`,
+        jokes
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("jokes", data);
+          // hook.innerHTML =
+          for (i in data) {
+            fill.insertAdjacentHTML("beforeend", `<li>${data[i].joke}</li>`);
+          }
+        })
+        .catch((error) => console.error("Error fetching jokes data:", error));
+    } else {
+      fetch(
+        `https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia?limit=${selectLimit.value}`,
+        trivia
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("trivia", data);
+          // hook.innerHTML =
+          for (i in data) {
+            fill.insertAdjacentHTML(
+              "beforeend",
+              `<li>${data[i].question}</li>`
+            );
+            document
+              .getElementById("listAnswer")
+              .insertAdjacentHTML("beforeend", `<li>${data[i].answer}</li>`);
+          }
+          document.getElementById("answerBtn").classList.remove("hide");
+        })
+        .catch((error) => console.error("Error fetching trivia data:", error));
+    }
+  } else {
+    alert("Please select amount");
+  }
+});
